e32c0d97857434f2034ca49b1066728d007f8907
Aristide-O <heavenly_7@hotmail.fr>
2020-06-24 22:49:12 +0200

    added Docto image

--- src/components/MainPageGlobal.js
+++ src/components/MainPageGlobal.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import  DoctoImg from "/Users/ariomega/Desktop/Doctolib/Doctolib-Hackathon48H/src/images/DoctoImg.jpg"
 import "./MainPageGlobal.css"
 
 class MainPageGlobal extends React.Component {
 render () {
     return (
         <div>
-            hello
-            hhhhhhhhhiiiii
-
+            <div>
+                <img className="img-MainPageGlobal" src={DoctoImg} alt="image photo doctor"/>
+            </div>
         </div>
     )
 }
