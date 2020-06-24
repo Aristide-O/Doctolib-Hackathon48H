84476572911f4e8d1d365452a711357bd0f650b9
Aristide-O <heavenly_7@hotmail.fr>
2020-06-24 22:24:44 +0200

    created MainPageGlobal components

---
+++ src/components/MainPageGlobal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./MainPageGlobal.css"
+
+class MainPageGlobal extends React.Component {
+render () {
+    return (
+        <div>
+            hello
+            hhhhhhhhhiiiii
+
+        </div>
+    )
+}
+}
+
+export default MainPageGlobal
