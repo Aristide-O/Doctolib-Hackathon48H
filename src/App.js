b8505a5ca0b3a4f6061b13a7ec5b889c8feb924f
Mohamed-Lababidi <lababidi.mohamed75@gmail.com>
2020-06-25 17:30:31 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import MainPageGlobal from './Components/Banniere/MainPageGlobal';
 import MainPage from './Components/MainPage/MainPage'
+import MedicalSurveillance from './Components/MedicalSurveillance/MedicalSurveillance';
+import ConnectionPage from './Components/ConnectionPage/ConnectionPage';
+import Footer from './Components/Footer/Footer';
 
 import './App.css';
+
 import Inscription from './Components/InscriptionPage/Inscription';
 import Navbar from './Components/Navbar/Navbar';
 import { Switch, Route } from 'react-router-dom';
-import MedicalSurveillance from './Components/MedicalSurveillance/MedicalSurveillance';
-import ConnectionPage from './Components/ConnectionPage/ConnectionPage';
 
 const App = () => {
   return (
@@ -15,17 +17,21 @@ const App = () => {
       <Switch>
         <Route exact path='/'>
           <Inscription />
+          <Footer />
         </Route>
         <Route path='/ConnectionPage'>
           <ConnectionPage />
+          <Footer />
         </Route>
         <Route path='/MedicalSurveillance'>
           <MedicalSurveillance />
+          <Footer />
         </Route>
         <Route path='/MainPage'>
           <MainPageGlobal />
           <Navbar />
           <MainPage />
+          <Footer />
         </Route>
       </Switch>
     </div>
