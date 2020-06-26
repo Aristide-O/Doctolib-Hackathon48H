7be879bb405ddd75eda33f70c591fadbb317a3d1
Aristide-O <heavenly_7@hotmail.fr>
2020-06-26 02:26:58 +0200

    imported buttons components in LandingPage.js

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
 import "./LandingPage.css"
 
-const LandingPage =() =>{
-    return (
-        <div>
-            <div>
-                
-            </div>
-        </div>
-    )
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
+export default function LandingPage() {
+  const classes = useStyles();
+
+  return (
+    <div className="landingPage-flex">
+      <div className="Buttons-landingPage">
+        <Button variant="outlined" size="large" color="primary" className={classes.margin}>
+          Public
+        </Button>
+        <Button variant="outlined" size="large" color="primary" className={classes.margin}>
+          Professionnal
+        </Button>
+      </div>
+    </div>
+  );
 }
-
-export default LandingPage
